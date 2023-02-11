164803963c187c66f77dc86410de443fb75f709a
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-10 22:00:53 -0300

    cancei de escrever tudo em ingles!

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 ```uma api feita em python com o intuito de substituir a requisicao de uma api de cep para o uso pessoal
 ```
+
+## Exemplos
+
+#### CepCheck.check()
+```python
+from cepchecking import CepCheck
+cep = "01001000"
+cepcheck = CepCheck(cep)
+
+cepcheck.check()
+```
+
+#### CepCheck.cep()
--- cepchecking/__init__.py
+++ cepchecking/__init__.py
@@ -4,9 +4,42 @@ from colorama import Fore
 class CepCheck:
     def __init__(self, cep):
         self.cep = cep
-        
+    
+    def todo(self):
+        r = requests.get(f'https://viacep.com.br/ws/{self.cep}/json/').json()
+        for k, v in r.items():
+            print(k, v)
+    
+    def localidade(self):
+        r = requests.get(f'https://viacep.com.br/ws/{self.cep}/json/').json()
+        print(r['localidade'])
+    
+    def logradouro(self):
+        r = requests.get(f"https://viacep.com.br/ws/{self.cep}/json/").json()
+        print(r['logradouro'])
+    
+    def bairro(self):
+        r = requests.get(f"https://viacep.com.br/ws/{self.cep}/json/").json()
+        print(r['bairro'])
+    
+    def comlemento(self):
+        r = requests.get(f"https://viacep.com.br/ws/{self.cep}/json/").json()
+        print(r['complemento'])
+    
+    def uf(self):
+        r = requests.get(f"https://viacep.com.br/ws/{self.cep}/json/").json()
+        print(r['uf'])
+
+class Cep:
+    def __init__(self, cep):
+        self.cep = cep
+    
     def check(self):
         if len(self.cep) <= 6:
             print(f"{Fore.LIGHTGREEN_EX}Is Valid!!")
         else:
             print(f"{Fore.LIGHTRED_EX}Not is valid!!")
+    
+    def formatCep(self):
+        r = requests.get(f"https://viacep.com.br/ws/{self.cep}/json/").json()
+        print(r['cep'])
